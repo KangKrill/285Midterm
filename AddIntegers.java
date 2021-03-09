44cb7ae5dc91bfd1af813e9301b88c6f98bd52f5
James <kowalczj@umich.edu>
2021-03-09 12:50:56 -0500

    atom message

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 50;
+        int first = 46;
+        int second = 23;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
